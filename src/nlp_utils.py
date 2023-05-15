e678b3d46726ea250b2afc841157bf7dd4138ee5
renatostosp <renato.stosp@gmail.com>
2023-05-15 00:32:06 -0300

    Atualizacao

--- src/nlp_utils.py
+++ src/nlp_utils.py
@@ -11,7 +11,18 @@ def preprocessing(resume_txt: str) -> str:
     new_resume_txt = new_resume_txt.replace('\n', ' ')
     new_resume_txt = unidecode.unidecode(new_resume_txt)
     doc = nlp(new_resume_txt)
-    tokens = [t.lemma_.lower() for t in doc if t.pos_ != 'PUNCT' and not t.is_stop]
+#    tokens = [t.lemma_.lower() for t in doc if t.pos_ != 'PUNCT' and not t.is_stop]
+    tokens = [t.text.lower() for t in doc if t.pos_ != 'PUNCT']
+    new_resume_txt = ' '.join(tokens).strip()
+    return new_resume_txt.lower()
+
+
+def preprocessing_neural(resume_txt: str) -> str:
+    new_resume_txt = re.sub(r'<.*?>', '', resume_txt)
+    new_resume_txt = re.sub(r'\s+', ' ', new_resume_txt)
+    new_resume_txt = new_resume_txt.replace('\n', ' ')
+    new_resume_txt = unidecode.unidecode(new_resume_txt)
+    doc = nlp(new_resume_txt)
     tokens = [t.text.lower() for t in doc if t.pos_ != 'PUNCT']
     new_resume_txt = ' '.join(tokens).strip()
     return new_resume_txt.lower()
